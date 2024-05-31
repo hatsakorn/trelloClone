e21989b5ee259b9a75b8bdb691422be8656855ab
hatsakorn <gornnb@gmail.com>
2024-05-31 17:14:09 +0700

    add css and try global css

--- app/(test)/layout.tsx
+++ app/(test)/layout.tsx
@@ -1,4 +1,9 @@
 const TestLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-rose-500">{children}</div>;
+  return (
+    <div className="h-full">
+      <div>This is nav bar</div>
+      {children}
+    </div>
+  );
 };
 export default TestLayout;
