56553cf1e6ca9184300f2741e330c4f75a5ac526
hatsakorn <gornnb@gmail.com>
2024-06-04 21:24:57 +0700

    feat(organization):not finish organization middleware afterAuth and if has auth

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,15 @@
+// import { OrganizationSwitcher } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs/server";
+
+const OrganizationIdPAge = () => {
+  //   const { userId, orgId } = auth();
+  return (
+    <div>
+      Organization Page
+      {/* <OrganizationSwitcher hidePersonal /> */}
+      {/* Organization {orgId} */}
+    </div>
+  );
+};
+
+export default OrganizationIdPAge;
