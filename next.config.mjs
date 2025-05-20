f943e0bc6a8d56413758480907cac15de446b2fd
hatsakorn <gornnb@gmail.com>
2025-05-20 12:51:09 +0700

    feat(deploy):try deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/2048-in-react",
-  output: "export",
-  reactStrictMode: true,
+  // basePath: "/2048-in-react",
+  // output: "export",
+  // reactStrictMode: true,
   images: {
     remotePatterns: [
       {
